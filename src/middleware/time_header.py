001237176029cfddf8183b365ec8e75e4276e3c7
Esmaeil kazemi <smk182018@gmail.com>
2023-01-14 17:29:23 +0330

    Feat(): create add process time header middleware

---
+++ src/middleware/time_header.py
@@ -0,0 +1,12 @@
+from fastapi import Request
+from src.main import app
+import time
+
+
+@app.middleware("http")
+async def add_process_time_header(request: Request, call_next):
+    start_time = time.time()
+    response = await call_next(request)
+    process_time = time.time() - start_time
+    response.headers["X-Process-Time"] = str(process_time)
+    return response
