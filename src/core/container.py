23977474cdd43b08bc559e03ffd07f8e86ab33ad
Esmaeil kazemi <smk182018@gmail.com>
2023-01-31 16:18:37 +0330

    Feat(): create Container class
    -create dependency injection config

---
+++ src/core/container.py
@@ -0,0 +1,50 @@
+from dependency_injector import containers, providers
+from src.config import get_setting
+from src.core.provider.neo import Neo4jConnection
+from src.repository import (
+    MovieRepository,
+    PeopleRepository,
+    UserRepository,
+    GenresRepository,
+)
+from src.services import (
+    MovieService,
+    AuthService,
+    RatingService,
+    UserService
+)
+
+settings = get_setting()
+
+
+class Container(containers.DeclarativeContainer):
+    wiring_config = containers.WiringConfiguration(
+        modules=[
+            "src.core.genre.router",
+            "src.core.movie.router",
+            "src.core.rating.router",
+            "src.core.user.router",
+            "src.core.auth.router",
+            "src.core.dependencies",
+        ]
+    )
+
+    db = providers.Singleton(Neo4jConnection,
+                             db_url=settings.neo4j_uri,
+                             uri=settings.neo4j_username,
+                             pwd=settings.neo4j_password
+                             )
+
+    movie_repository = providers.Factory(MovieRepository,
+                                         session_factory=db.provided.session)
+    user_repository = providers.Factory(UserRepository,
+                                        session_factory=db.provided.session)
+
+    auth_service = providers.Factory(AuthService,
+                                     user_repository=user_repository)
+    rate_service = providers.Factory(RatingService,
+                                     movie_repository=MovieRepository)
+    movie_service = providers.Factory(MovieService,
+                                      movie_repository=movie_repository)
+    user_service = providers.Factory(UserService,
+                                     user_repository=user_repository)
