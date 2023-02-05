aea04c324067a22f17170b554b392790f2de1166
Esmaeil kazemi <smk182018@gmail.com>
2023-02-05 17:32:39 +0330

    Fix(): import GenreService

--- src/services/__init__.py
+++ src/services/__init__.py
@@ -4,3 +4,4 @@ from src.services.user import UserService
 from src.services.rate import RatingService
 from src.services.people import PeopleService
 from src.services.favorite import FavoriteService
+from src.services.genre import GenreService
