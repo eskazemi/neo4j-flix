974abb7b28b39f6b07393a459c96caaf636a3c7e
Esmaeil kazemi <smk182018@gmail.com>
2023-02-06 13:57:33 +0330

    Feat(): add functions
    -create get movies function
    -create find genre function

--- src/repository/genre.py
+++ src/repository/genre.py
@@ -12,3 +12,42 @@ class GenresRepository(BaseRepository):
     def __init__(self, session_factory: Callable[..., AbstractContextManager[Session]]):
         self.session_factory = session_factory
         super().__init__(session_factory)
+
+    def get_movies(self):
+        with self.session_factory() as session:
+            result = session.run("""
+                       MATCH (g:Genre)
+                       WHERE g.name <> '(no genres listed)'
+                       CALL {
+                           WITH g
+                           MATCH (g)<-[:IN_GENRE]-(m:Movie)
+                           WHERE m.imdbRating IS NOT NULL AND m.poster IS NOT NULL
+                           RETURN m.poster AS poster
+                           ORDER BY m.imdbRating DESC LIMIT 1
+                       }
+                       RETURN g {
+                           .*,
+                           movies: size((g)<-[:IN_GENRE]-(:Movie)),
+                           poster: poster
+                       } AS genre
+                       ORDER BY g.name ASC
+                   """)
+
+            return [g.value(0) for g in result]
+
+    def find_genre(self, name):
+        with self.session_factory() as session:
+            first = session.run("""
+                        MATCH (g:Genre {name: $name})<-[:IN_GENRE]-(m:Movie)
+                        WHERE m.imdbRating IS NOT NULL AND m.poster IS NOT NULL AND g.name <> '(no genres listed)'
+                        WITH g, m
+                        ORDER BY m.imdbRating DESC
+                        WITH g, head(collect(m)) AS movie
+                        RETURN g {
+                            .name,
+                            movies: size((g)<-[:IN_GENRE]-()),
+                            poster: movie.poster
+                        } AS genre
+                    """, name=name).single()
+            return first
+
