9308c04d5e66e9d09ce184a25e50f5679b3aa7d4
Esmaeil kazemi <smk182018@gmail.com>
2023-02-06 14:16:16 +0330

    fix(): update routers

--- src/routers/account.py
+++ src/routers/account.py
@@ -3,8 +3,9 @@ from dependency_injector.wiring import (
     Provide,
     inject
 )
+from fastapi import Query
 from src.services.favorite import FavoriteService
-from src.services.rate import RatingService
+from src.services.rating import RatingService
 from fastapi import (
     APIRouter,
     Depends,
@@ -18,27 +19,58 @@ account_routes = APIRouter(
 
 @account_routes.get('/favorites')
 @inject
-def get_favorites(service: FavoriteService = Depends(
-    Provide[Container.auth_service])):
-    pass
+def get_favorites(user_id: str,
+                  skip: int = 0,
+                  limit: int = 10,
+                  order: str = 'ASK',
+                  sort: str = 'title',
+                  service: FavoriteService = Depends(
+                      Provide[Container.auth_service])):
+    # Get search parameters
+
+    output = service.all(user_id, sort, order, limit, skip)
+    return output
+
+    # return jsonify(output)
 
 
 @account_routes.post('/favorites/<movie_id>')
 @inject
-def add_favorite(movie_id, service: FavoriteService = Depends(
+def add_favorite(movie_id, user_id, service: FavoriteService = Depends(
     Provide[Container.auth_service])):
-    pass
+    # Get user ID from JWT
+    # user_id = current_user["sub"]
+
+    output = service.add(user_id, movie_id)
+    return output
+    # Return the output
+    # return jsonify(output)
 
 
 @account_routes.delete('/favorites/<movie_id>')
 @inject
-def delete_favorite(movie_id, service: FavoriteService = Depends(
+def delete_favorite(movie_id, user_id, service: FavoriteService = Depends(
     Provide[Container.auth_service])):
-    pass
+    # Get user ID from JWT
+    # user_id = current_user["sub"]
+
+    output = service.remove(user_id, movie_id)
+    return output
+    # Return the output
+    # return jsonify(output)
 
 
 @account_routes.post('/ratings/<movie_id>')
 @inject
-def save_rating(movie_id, service: RatingService = Depends(
+def save_rating(movie_id, rating: int = Query(...), service: RatingService = Depends(
     Provide[Container.auth_service])):
-    pass
+    # Get user ID from JWT
+    # user_id = current_user["sub"]
+
+    # Save the rating
+    output = service.add(user_id, movie_id, rating)
+
+    # Return the output
+    return output
+    # return jsonify(output)
+
