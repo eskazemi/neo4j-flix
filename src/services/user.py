a7d43bcc1347d1edb42b7a5feaad2034331ad446
Esmaeil kazemi <smk182018@gmail.com>
2023-02-07 10:29:10 +0330

    Fix(): add get_profile function

--- src/services/user.py
+++ src/services/user.py
@@ -2,15 +2,12 @@ from src.repository import UserRepository
 from src.core.base_service import BaseService
 
 
-
-
 class UserService(BaseService):
     def __init__(self, user_repository: UserRepository):
         self.user_repository = user_repository
         super().__init__(user_repository)
 
     def check_user_exists(self, unique_attr: str):
-
         return self.user_repository.check_user_exists(unique_attr)
 
     def get_user(self, user: str):
@@ -21,3 +18,8 @@ class UserService(BaseService):
         """
         return self.user_repository.get_user(user)
 
+    def get_profile(self, user_id):
+        return self.user_repository.get_info_user(user_id)
+
+    def update_profile(self, attributes, user_id: str):
+        return self.user_repository.update(attributes, user_id)
